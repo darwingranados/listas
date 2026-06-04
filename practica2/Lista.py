2feae45edb4a6ec047c4337724b11f60c2e3aa56
darwingranados <20323757@clases.edu.sv>
2026-06-04 07:39:40 -0600

    Create Lista.py

---
+++ practica2/Lista.py
@@ -0,0 +1,8 @@
+istas=["Rodrigo","Juan"]#constantemente agregando información
+tupla=("Rodrigo","Juan")
+
+videojuego = []
+#nombres, Genero, años
+videojuego.append(("final fantasy 7", "JRPG", 1997))
+videojuego.append(("outer wilds","aventura",2019))
+print(videojuego)
