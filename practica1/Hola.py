6ccf5a5abf81ce2f3fd8b59d39de9b21dfefeff4
darwingranados <20323757@clases.edu.sv>
2026-06-04 07:40:46 -0600

    Update Hola.py

--- practica1/Hola.py
+++ practica1/Hola.py
@@ -1,8 +1,2 @@
-listas=["Rodrigo","Juan"]#constantemente agregando información
-tupla=("Rodrigo","Juan")
-
-videojuego = []
-#nombres, Genero, años
-videojuego.append(("final fantasy 7", "JRPG", 1997))
-videojuego.append(("outer wilds","aventura",2019))
-print(videojuego)
+print("Python")
+print("Como esta")
